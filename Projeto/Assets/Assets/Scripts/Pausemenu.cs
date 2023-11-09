96cb739655fcd5e5a963319a85cb271bc0bd179b
NicolasGMF <nicholasfmartins01@gmail.com>
2023-11-09 03:14:17 -0300

    Menupause
    Tentando ajustar o menu de pause

---
+++ Projeto/Assets/Assets/Scripts/Pausemenu.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+    private string cenas = "Menu";
+    public static bool Parandojogo = false;
+    public GameObject pausemenuUi;
+    void Update()
+    {   
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(Parandojogo)
+            {
+                Continuar();
+            }else
+            {
+                pause();
+            }
+        }
+
+        
+    }
+      public void Continuar()
+    {
+        pausemenuUi.SetActive(false);
+        Time.timeScale = 1.0f;
+        Parandojogo = false;
+    }
+    void pause()
+    {
+        pausemenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        Parandojogo=true;
+    }
+    public void carregarmenu()
+    {
+        SceneManager.LoadScene(cenas);
+    }
+    public void sairdojogo() 
+    {
+        Application.Quit();
+        Debug.Log("Morri");
+    }
+}
